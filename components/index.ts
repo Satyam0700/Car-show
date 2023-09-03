36d3253ce8b14542bfd97be498b817c533004b89
Satyam 0700 <gamerzhind199@gmail.com>
2023-09-03 19:44:00 +0530

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import CarCard from "./CarCard";
+import CarDetails from "./CarDetails";
+import CustomButton from "./CustomButton";
+import CustomFilter from "./CustomFilter";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
+import SearchManufacturer from "./SearchManufacturer";
+import ShowMore from "./ShowMore";
+
+export {
+  Hero,
+  CustomButton,
+  Navbar,
+  Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
+  CarCard,
+  CarDetails,
+  ShowMore
+};
